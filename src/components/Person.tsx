b1ef602fc52fe034fc6ea110b02f85a6c9355072
Tomasz Bortacki <tomasz.bortacki99@gmail.com>
2021-03-27 20:47:01 +0100

    Adding https protocol switch

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -31,7 +31,7 @@ const Person = ({
       if (exist) setTitles((prevState) => [...prevState, exist]);
       else
         await axios
-          .get(film)
+          .get(film.replace("http", window.location.protocol))
           .then((res) => {
             const title = res.data.title;
             setTitleBase((prevState: any) => [
